cabcc4649ce9b04853a11449c4bc4b209ee38330
kokoriru <kokorirury@yahoo.co.jp>
2015-12-17 00:38:17 +0900

    テストする順番を変更

--- test/controllers/top_controller_test.rb
+++ test/controllers/top_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class TopControllerTest < ActionController::TestCase
   test "index ページへ正常にアクセスできること" do
     get :index
-    assert_template :index
     assert_response :success
+    assert_template :index
   end
 end
