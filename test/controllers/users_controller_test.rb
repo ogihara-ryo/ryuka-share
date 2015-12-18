704901b1404560e61dd865e950dff53b61b8d932
kokoriru <kokorirury@yahoo.co.jp>
2015-12-18 21:48:28 +0900

    users#index ページのテストを作成

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+  test "index ページへ正常にアクセスできること" do
+    get :index
+    assert_response :success
+    assert_template :index
+  end
+end
