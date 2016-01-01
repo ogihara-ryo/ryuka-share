e24202e7c8c1cac5a5eb64a7582433b4beb16c1e
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-01-01 12:28:46 +0900

    config/routes.rb に annotate 実行

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
+# == Route Map
+#
+#    Prefix Verb   URI Pattern               Controller#Action
+#      root GET    /                         top#index
+#     users GET    /users(.:format)          users#index
+#           POST   /users(.:format)          users#create
+#  new_user GET    /users/new(.:format)      users#new
+# edit_user GET    /users/:id/edit(.:format) users#edit
+#      user GET    /users/:id(.:format)      users#show
+#           PATCH  /users/:id(.:format)      users#update
+#           PUT    /users/:id(.:format)      users#update
+#           DELETE /users/:id(.:format)      users#destroy
+#
+
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
